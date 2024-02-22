3e478de0be199f46330c18808530f993e278b950
unknown <ahteshamshaykhi@gmail.com>
2024-02-22 22:12:34 +0500

    its the little things

---
+++ app.js
@@ -0,0 +1,175 @@
+//(Alert)
+
+alert("hello world");
+
+alert("Error please enter a valid Passward");
+
+alert("Welcome to JS land\n happy coding");
+
+alert("welcome to js land");
+
+alert("happy coding!\n prevent this creating additnol dialogs.");
+
+console.log ("hello i can run js through my web browser console");
+
+
+//VARIABLES FOR STRINGS
+
+var username;
+
+
+var username = "Ahtesham Ahmed";
+
+
+var message;
+var message = "hello world";
+alert(message);
+
+var firstname = "john doe";
+
+var age = "20 years old";
+
+var course = "web and mobile development";
+
+var bio = firstname + " " + age +" " + course;
+alert(bio);
+
+
+var eat = ("pizza\n pizz\n piz \n pi \n p");
+var eaten = "eat"
+alert(eat);
+
+
+var email = ("my email is ahteshamshaikh@gmail.com");
+var mail = "email"
+alert(email);
+
+var book = ("A smarter way to learn javascript");
+var a = "book";
+alert(book);
+
+
+//document.write("Yah! i can write html content through JavaScript");
+
+var b = ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+var c = "b";
+alert(b);
+
+
+
+//VARIABLES FOR NUMBERS
+
+
+var age = ("my age is 20");
+var d = "age";
+alert(age);
+
+
+var birthyear = "my birthyear is 2000.";
+document.write(birthyear);
+
+
+
+var visitorname = "John Doe";
+var producttitle = "order5,tshirt";
+var storename = "on XYZ Clothing store.”";
+var final = visitorname + " " + producttitle + " " + storename;
+document.write(final);
+
+
+
+//chapter 01 (Alert)
+
+// Q1
+
+alert (" Ahtesham ")
+
+alert ("Ahmed")
+
+alert ("ahteshamshaykhi@gmail.com")
+
+alert (92123456645)
+
+// Q2 
+
+alert ("you are learning JavaScript")
+
+// Q3
+
+alert ("HELLO WORLD")
+
+// chap no 02 (Variale of string)
+
+// Q1
+
+var camelCase;
+
+//Q2
+
+var g = m
+var h = "hello world";
+
+//Q3
+
+var teamname = "Ninja";
+var t = "teamname";
+
+alert (teamname)
+
+//Q4
+
+var bestman = "Charlie";
+var m = "bestman";
+alert (bestman)
+
+
+
+// chap no 03(Variable of Number)
+
+//Q1
+
+
+var x = "caseQty";
+
+//Q2
+
+var caseQty = "144";
+
+//Q3
+
+var num = 9;
+var num2 = 4;
+var c = num + num2;
+alert(c)
+
+
+//Q4
+
+var a = "ahmed";
+var b = 5;
+var c = a + b;
+document.write(c);
+
+
+//Q5
+ 
+var merchTotal = 100;
+var shippingCharge = 10;
+
+var orderTotal = merchTotal + shippingCharge;
+alert(orderTotal)
+
+//Q6
+
+var d = 100;
+var e = 400;
+var g = d + e;
+alert(g);
+
+
+
+
+
+
+
+
